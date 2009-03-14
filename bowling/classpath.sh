65545eaff681a47ad78d97cc6842a5dfbf658a13
Claudio Clemens <asturio@gmx.net>
2009-03-14 10:07:47 +0000

    Added targets: doc, test, checkstyle Added needed classes for junit and checkstyle
    git-svn-id: svn+ssh://boromir.karameikos.bogus/opt/repos/svnroot/conject/trunk@14 0f4e6a8a-931a-46bb-b6e4-0af5db81de1f

--- bowling/classpath.sh
+++ bowling/classpath.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-export CLASSPATH=$CLASSPAtH:/usr/share/java/junit4.jar:build
+NEEDED="junit4 commons-beanutils commons-logging antlr commons-collections"
+for JAR in $NEEDED
+do
+    CLASSPATH=$CLASSPATH:/usr/share/java/$JAR.jar
+done
+CLASSPATH=$CLASSPATH:build
+export CLASSPATH
